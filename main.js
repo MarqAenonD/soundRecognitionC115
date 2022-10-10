9c93a307f0a63dee8c02f03e493333ad69bc87e2
MarqAenonD <106565814+MarqAenonD@users.noreply.github.com>
2022-10-10 11:21:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+//https://teachablemachine.withgoogle.com/models/gOPLjD7yT/
+
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/gOPLjD7yT/model.json", modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults)
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = "the AI hears: " + results[0].label;
+        document.getElementById("result_confidence").innerHTML = "accuracy: " + (results[0].confidence * 100).toFixed(2) + "%";
+        document.getElementById("result_label").style.color = "rgb(" + random_number_r + ", " + random_number_g + ", " + random_number_b + ")";
+        document.getElementById("result_confidence").style.color = "rgb(" + random_number_r + ", " + random_number_g + ", " + random_number_b + ")";
+
+        img = document.getElementById("alien1");
+
+        if (results[0].label == "bark") {
+            img.src = "dog.jpg";
+        } else if (results[0].label == "chirp") {
+            img.src = "bird.png";
+        } else if (results[0].label == "meow") {
+            img.src = "cat.jpg";
+        } else {
+            img.src = "listen.png";
+        };
+    }
+}
